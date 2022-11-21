1218027716b7279cc26bbb413597c3240116783d
Knut Andreas Meyer <knutam@gmail.com>
2022-11-21 14:02:18 +0100

    Fix CI try 2

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,8 +17,9 @@ using Test
         # Note: Not optimal as we should actually check the printed message
         cb = CellBuffer(dh, cv, nothing)    # material=nothing not supported
         cb_ad = AutoDiffCellBuffer(states, dh, cv, nothing)
-        @test_throws "element_residual!" doassemble!(start_assemble(K,r), cb, states, dh, a)
-        @test_throws "element_residual!" doassemble!(start_assemble(K,r), cb_ad, states, dh, a)
+        errorspec = VERSION >= v"1.8" ? "element_residual!" : Exception
+        @test_throws errorspec doassemble!(start_assemble(K,r), cb, states, dh, a)
+        @test_throws errorspec doassemble!(start_assemble(K,r), cb_ad, states, dh, a)
         
         # Check error when trying to insert a dual number into the state variables
         # Note: Not optimal as we should actually check the printed message
@@ -29,7 +30,9 @@ using Test
             state[1] = first(ae) # Not allowed, must be state[1] = ForwardDiff.value(first(ae))
         end
         cb_dualissue = AutoDiffCellBuffer(states_dualissue, dh, cv, _TestMaterial())
-        @test_throws ["Float64", "Dual"] doassemble!(start_assemble(K,r), cb_dualissue, states_dualissue, dh, a)
+        errorspec = VERSION >= v"1.8" ? ["Float64", "Dual"] : Exception
+        @test_throws errorspec doassemble!(start_assemble(K,r), cb_dualissue, states_dualissue, dh, a)
+        
     end
 
 end
