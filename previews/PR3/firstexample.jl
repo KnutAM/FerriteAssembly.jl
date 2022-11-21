c9f5c5b4422eea80102c70694960a01caecd1d37
Documenter.jl <documenter@juliadocs.github.io>
2022-11-21 08:43:34 +0000

    build based on 5b7fd94

---
+++ previews/PR3/firstexample.jl
@@ -0,0 +1,83 @@
+using Ferrite, FerriteAssembly, BenchmarkTools
+dh = DofHandler(generate_grid(Quadrilateral, (100, 100))); push!(dh, :u, 1); close!(dh)
+cellvalues = CellScalarValues(QuadratureRule{2, RefCube}(2), Lagrange{2, RefCube, 1}())
+
+struct ThermalMaterial end
+
+function FerriteAssembly.element_routine!(
+    Ke::AbstractMatrix, re::AbstractVector, state,
+    ae::AbstractVector, material::ThermalMaterial, cellvalues::CellScalarValues,
+    dh_fh, Δt, buffer::CellBuffer
+    )
+    n_basefuncs = getnbasefunctions(cellvalues)
+    # Loop over quadrature points
+    for q_point in 1:getnquadpoints(cellvalues)
+        dΩ = getdetJdV(cellvalues, q_point)
+        for i in 1:n_basefuncs
+            δN  = shape_value(cellvalues, q_point, i)
+            ∇δN = shape_gradient(cellvalues, q_point, i)
+            # Add body load contribution to re
+            re[i] += -δN * dΩ
+            # Loop over trial shape functions
+            for j in 1:n_basefuncs
+                ∇N = shape_gradient(cellvalues, q_point, j)
+                # Add contribution to Ke
+                Ke[i, j] += (∇δN ⋅ ∇N) * dΩ
+            end
+        end
+    end
+end
+
+K = create_sparsity_pattern(dh)
+r = zeros(ndofs(dh))
+
+states = create_states(dh)
+
+cellbuffer = CellBuffer(dh, cellvalues, ThermalMaterial())
+
+assembler = start_assemble(K,r)
+doassemble!(assembler, cellbuffer, states, dh)
+
+colors = create_coloring(dh.grid)
+
+cellbuffers = create_threaded_CellBuffers(CellBuffer(dh, cellvalues, ThermalMaterial()))
+assemblers = create_threaded_assemblers(K, r)
+
+doassemble!(assemblers, cellbuffers, states, dh, colors)
+
+struct ThermalMaterialAD end
+
+function FerriteAssembly.element_residual!(
+    re::AbstractVector, state,
+    ae::AbstractVector, material::ThermalMaterialAD, cellvalues,
+    dh_fh::Union{DofHandler,FieldHandler}, Δt, buffer
+    )
+    n_basefuncs = getnbasefunctions(cellvalues)
+
+    for q_point in 1:getnquadpoints(cellvalues)
+
+        dΩ = getdetJdV(cellvalues, q_point)
+        ∇u = function_gradient(cellvalues, q_point, ae)
+
+        for i in 1:n_basefuncs
+            δN  = shape_value(cellvalues, q_point, i)
+            ∇δN = shape_gradient(cellvalues, q_point, i)
+
+            re[i] += (∇δN ⋅ ∇u - δN) * dΩ
+        end
+    end
+end
+
+a = zeros(ndofs(dh))
+cellbuffer2 = CellBuffer(dh, cellvalues, ThermalMaterialAD())
+assembler = start_assemble(K,r)
+doassemble!(assembler, cellbuffer2, states, dh, a)
+
+@btime doassemble!(assembler, $cellbuffer, $states, $dh) setup=(assembler=start_assemble(K,r))
+@btime doassemble!(assembler, $cellbuffer2, $states, $dh) setup=(assembler=start_assemble(K,r))
+
+cellbuffer3 = FerriteAssembly.AutoDiffCellBuffer(states, dh, cellvalues, ThermalMaterialAD())
+@btime doassemble!(assembler, $cellbuffer3, $states, $dh) setup=(assembler=start_assemble(K,r))
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ previews/PR3/literate/plasticity.jl
@@ -0,0 +1,33 @@
+# # Plasticity
+# This example shows how any material following the 
+# [`MaterialModelsBase.jl`](https://github.com/KnutAM/MaterialModelsBase.jl)
+# interface can be assembled with `FerriteAssembly.jl`. 
+# We start by the required packages
+using Tensors, MaterialModelsBase, Ferrite, FerriteAssembly
+
+include("J2Plasticity.jl")
+
+include("MaterialModelsBaseElement.jl")
+
+# ## Assembly
+# With all required functions defined, we can now setup and assemble the finite element problem 
+material = J2Plasticity(200.0e9, 0.3, 200.0e6, 10.0e9);
+grid = generate_grid(Tetrahedron, (20,2,4), zero(Vec{3}), Vec((10.0,1.0,1.0)));
+cellvalues = CellVectorValues(
+    QuadratureRule{3,RefTetrahedron}(2), Lagrange{3, RefTetrahedron, 1}());
+dh = DofHandler(grid); push!(dh, :u, 3); close!(dh); # Create dofhandler
+K = create_sparsity_pattern(dh);
+r = zeros(ndofs(dh));
+
+# Using the `create_states` function, we can easily create the storage of state variables 
+# suitable for the chosen dof handler 
+states = create_states(dh, _->initial_material_state(material), cellvalues);
+
+# And then we create the buffer for saving cell-related variables
+buffer = CellBuffer(dh, cellvalues, material, nothing, get_cache(material));
+
+# Finally, with an initial guess of displacements, `a`, 
+# we can create the assembler and do the assembly
+a = zeros(ndofs(dh))
+assembler = start_assemble(K,r)
+doassemble!(assembler, buffer, states, dh, a);
