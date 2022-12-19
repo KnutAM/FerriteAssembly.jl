5e0cb407372df335fd2028fac47939abc514aba5
Documenter.jl <documenter@juliadocs.github.io>
2022-12-19 17:31:59 +0000

    build based on a85718c

---
+++ previews/PR7/examples/mixed_materials.jl
@@ -0,0 +1,53 @@
+using Tensors, MaterialModelsBase, Ferrite, FerriteAssembly
+include("J2Plasticity.jl");
+include("MaterialModelsBaseElement.jl");
+
+struct ElasticMaterial{T<:SymmetricTensor{4,3}} <: AbstractMaterial
+    D::T
+end
+function ElasticMaterial(;E, ν)
+    D, _, _ = elastic_conversion(E, ν)
+    return ElasticMaterial(D)
+end;
+
+function MaterialModelsBase.material_response(
+    material::ElasticMaterial, ϵ::SymmetricTensor{2,3}, state,
+    Δt, cache=get_cache(material), args...; kwargs...)
+    σ = material.D ⊡ ϵ
+    return σ, material.D, NoMaterialState()
+end;
+
+materials = Dict(
+    "elastic" => ElasticMaterial(E=200.0e9, ν=0.3),
+    "plastic" => J2Plasticity(200.0e9, 0.3, 200.0e6, 10.0e9));
+grid = generate_grid(Tetrahedron, (20,2,4), zero(Vec{3}), Vec((10.0,1.0,1.0)));
+cellvalues = CellVectorValues(
+    QuadratureRule{3,RefTetrahedron}(2), Lagrange{3, RefTetrahedron, 1}());
+dh = DofHandler(grid); push!(dh, :u, 3); close!(dh); # Create dofhandler
+K = create_sparsity_pattern(dh);
+r = zeros(ndofs(dh));
+
+addcellset!(grid, "elastic", x -> x[1] <= 5.0+eps())
+addcellset!(grid, "plastic", setdiff(1:getncells(grid), getcellset(grid,"elastic")));
+
+states = create_states(dh, materials, cellvalues);
+
+caches = Dict(key=>get_cache(mat) for (key,mat) in materials)
+
+buffers = setup_cellbuffer(dh, cellvalues, materials, nothing, caches);
+
+a = zeros(ndofs(dh))
+assembler = start_assemble(K, r)
+doassemble!(assembler, buffers, states, dh, a);
+
+using Test #hide
+K_ref = create_sparsity_pattern(dh); #hide
+r_ref = zeros(ndofs(dh)); #hide
+states_ref = create_states(dh, materials["elastic"], cellvalues); #hide
+a_ref = zeros(ndofs(dh)) #hide
+assembler_ref = start_assemble(K_ref,r_ref) #hide
+doassemble!(assembler_ref, buffers["elastic"], states_ref, dh, a_ref); #hide
+@test K ≈ K_ref; #hide
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ previews/PR7/literate/MaterialModelsBaseElement.jl
@@ -0,0 +1,32 @@
+# ## Element routine
+# The element routine for any material that follows the `MaterialModelsBase.jl` interface 
+# can be coded as follows:
+function FerriteAssembly.element_routine!(
+    Ke::AbstractMatrix, re::AbstractVector, state::Vector{<:AbstractMaterialState}, 
+    ae::AbstractVector, material::AbstractMaterial, cellvalues::CellVectorValues, dh_fh, Δt, buffer::CellBuffer)
+    cache = FerriteAssembly.get_cache(buffer)
+    n_basefuncs = getnbasefunctions(cellvalues)
+    for q_point in 1:getnquadpoints(cellvalues)
+        ## For each integration point, compute stress and material stiffness
+        ϵ = function_symmetric_gradient(cellvalues, q_point, ae) # Total strain
+        σ, D, state[q_point] = material_response(material, ϵ, state[q_point], Δt, cache)
+
+        dΩ = getdetJdV(cellvalues, q_point)
+        for i in 1:n_basefuncs
+            δϵ = shape_symmetric_gradient(cellvalues, q_point, i)
+            re[i] += (δϵ ⊡ σ) * dΩ ## add internal force to residual
+            for j in 1:n_basefuncs
+                Δϵ = shape_symmetric_gradient(cellvalues, q_point, j)
+                Ke[i, j] += δϵ ⊡ D ⊡ Δϵ * dΩ
+            end
+        end
+    end
+end;
+# Note that the boundary traction was not included, as this can be handled separately
+# with [FerriteNeumann.jl](https://github.com/KnutAM/FerriteNeumann.jl)
+
+# ## Initial states
+# The initial states are created by overloading the `create_cell_state` function 
+function FerriteAssembly.create_cell_state(material::AbstractMaterial, cellvalues, x, ae)
+    return [initial_material_state(material) for _ in 1:getnquadpoints(cellvalues)]
+end
