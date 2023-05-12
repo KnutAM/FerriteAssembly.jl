857820070bb95d500337e87a788eb5f357da49c1
Knut Andreas Meyer <knutam@gmail.com>
2023-05-12 17:39:41 +0200

    Remove unused function and unexpose internal dispatch on doassemble

--- src/assembly.jl
+++ src/assembly.jl
@@ -1,11 +1,11 @@
 """
     doassemble!(assembler, new_states::Dict{Int}, buffer::AbstractDomainBuffer; a=nothing, aold=nothing, old_states=nothing, Δt=NaN)
 
-Use `assembler` to assemble a single domain described by `buffer`, and update `new_states` if dictaded by the assembler. 
+Use `assembler` to assemble a single domain described by `buffer`, and update `new_states` if dictated by the assembler. 
 
     doassemble!(assembler, new_states::Dict{String}, buffers::Dict{String,AbstractDomainBuffer}; a=nothing, aold=nothing, old_states=nothing, Δt=NaN)
 
-Use `assembler` to assemble all domains described by `buffers`, and update `new_states` if dictaded by the assembler.
+Use `assembler` to assemble all domains described by `buffers`, and update `new_states` if dictated by the assembler.
 
 The keyword arguments work as follows:
 - `a, aold`: If these are `nothing`, then `NaN` values are given to the element routine for the corresponding entry.
@@ -19,9 +19,6 @@ function doassemble!(assembler, new_states, buffer::ThreadedDomainBuffer; kwargs
     threaded_assembler = TaskLocals(assembler)
     assemble_domain!(threaded_assembler, new_states, buffer; kwargs...)
 end
-function doassemble!(assembler::TaskLocals, new_states, buffer::ThreadedDomainBuffer; kwargs...)
-    assemble_domain!(assembler, new_states, buffer; kwargs...)
-end
 
 # Assemble multiple domains
 function doassemble!(assembler, new_states, buffers::Dict{String,<:DomainBuffer}; old_states=nothing, kwargs...)
@@ -32,9 +29,9 @@ function doassemble!(assembler, new_states, buffers::Dict{String,<:DomainBuffer}
 end
 function doassemble!(assembler, new_states, buffers::Dict{String,<:ThreadedDomainBuffer}; kwargs...)
     threaded_assembler = TaskLocals(assembler)
-    doassemble!(threaded_assembler, new_states, buffers; kwargs...)
+    threaded_doassemble!(threaded_assembler, new_states, buffers; kwargs...)
 end
-function doassemble!(assembler::TaskLocals, new_states, buffers::Dict{String,<:ThreadedDomainBuffer}; old_states=nothing, kwargs...)
+function threaded_doassemble!(assembler::TaskLocals, new_states, buffers::Dict{String,<:ThreadedDomainBuffer}; old_states=nothing, kwargs...)
     for key in keys(new_states)
         domain_old_states = isnothing(old_states) ? nothing : fast_getindex(old_states, key)
         assemble_domain!(assembler, new_states[key], buffers[key]; old_states=domain_old_states, kwargs...)
