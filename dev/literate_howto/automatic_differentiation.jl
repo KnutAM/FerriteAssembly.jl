257f7b3787795b30eb7ea7b63c97f8f902715d37
Documenter.jl <documenter@juliadocs.github.io>
2023-09-28 17:56:23 +0000

    build based on a4822ec

---
+++ dev/literate_howto/automatic_differentiation.jl
@@ -0,0 +1,88 @@
+# # Automatic differentiation
+# **Executive summary:** Define `element_residual!` and call `setup_domainbuffer(domain; autodiffbuffer=true)`.
+# 
+# Here, we show how to define only the `element_residual!` function, which 
+# does not calculate the element stiffness matrix, `Ke`, and then let `FerriteAssembly`'s
+# Automatic Differentiation (AD) functionality (that builds on `ForwardDiff.jl`)
+# calculate `Ke`. To compare with, we'll use the `StationaryFourier` example element that 
+# does define an `element_routine!` that calculates `Ke` explicitly. 
+using Ferrite, FerriteAssembly, BenchmarkTools
+import FerriteAssembly.ExampleElements: StationaryFourier
+
+# First, we'll define a new material, for which we only implement the `element_residual!`
+struct StationaryFourierAD
+    k::Float64 # Thermal conductivity
+end
+material = StationaryFourierAD(1.0)
+
+function FerriteAssembly.element_residual!(re, state, ae, 
+        material::StationaryFourierAD, cellvalues, cellbuffer
+        )
+    n_basefuncs = getnbasefunctions(cellvalues)
+    ## Loop over quadrature points
+    for q_point in 1:getnquadpoints(cellvalues)
+        ## Get the quadrature weight
+        dΩ = getdetJdV(cellvalues, q_point)
+        ∇u = function_gradient(cellvalues, q_point, ae)
+        ## Loop over test shape functions
+        for i in 1:n_basefuncs
+            ∇δN = shape_gradient(cellvalues, q_point, i)
+            ## re = fint - fext
+            re[i] += material.k*(∇δN ⋅ ∇u) * dΩ
+        end
+    end
+end;
+
+# Then, we create a standard Ferrite setup:
+grid = generate_grid(Quadrilateral, (100, 100))
+dh = DofHandler(grid); add!(dh, :u, 1); close!(dh)
+cellvalues = CellScalarValues(QuadratureRule{2, RefCube}(2), Lagrange{2, RefCube, 1}());
+K = create_sparsity_pattern(dh)
+r = zeros(ndofs(dh));
+
+# The assembly can then be done as usual
+domain = DomainSpec(dh, material, cellvalues)
+buffer = setup_domainbuffer(domain);
+
+# But for doing automatic differentiation, we need 
+# actual values for the degrees of freedom, so they 
+# must be passed to the work function,
+a = zeros(ndofs(dh))
+assembler = start_assemble(K, r)
+work!(assembler, buffer; a=a);
+
+# ## Performance
+# Using the simplest setup for AD is quite slow, due to lack of pre-allocations. 
+# If we would use the builtin material instead, which doesn't use AD, it is much faster:
+domain_builtin = DomainSpec(dh, StationaryFourier(1.0), cellvalues)
+buffer_builtin = setup_domainbuffer(domain_builtin);
+# Explicitly defining the element stiffness was a lot faster and has less allocations:
+if get(ENV, "CI", "false") == "true"            #hide
+@btime work!($assembler, $buffer; a=$a)
+@btime work!($assembler, $buffer_builtin; a=$a)
+end                                             #hide
+
+# However, FerriteAssembly comes with a special `cellbuffer::AutoDiffCellBuffer` 
+# for speeding up automatic differentiation. 
+# Simply say that you want to use this during setup to improve the performance when 
+# using AD
+buffer_ad = setup_domainbuffer(domain; autodiffbuffer=true)
+if get(ENV, "CI", "false") == "true"            #hide
+@btime work!($assembler, $buffer_ad; a=$a)
+end                                             #hide
+
+# Test that all methods give the same stiffness #src
+assembler = start_assemble(K, r)                #hide
+work!(assembler, buffer; a=a);                  #hide
+K1 = copy(K);                                   #hide
+assembler = start_assemble(K, r)                #hide
+work!(assembler, buffer_builtin; a=a);          #hide
+K2 = copy(K);                                   #hide
+assembler = start_assemble(K, r)                #hide
+work!(assembler, buffer_ad; a=a);               #hide
+K3 = copy(K);                                   #hide
+                                                #hide
+using Test                                      #hide
+@test K1.nzval !== K2.nzval                     #hide
+@test K1 ≈ K2 ≈ K3                              #hide
+nothing;                                        #hide
---
+++ dev/tutorials/heat_equation.jl
@@ -0,0 +1,54 @@
+using Ferrite, FerriteAssembly
+grid = generate_grid(Quadrilateral, (20, 20))
+dh = DofHandler(grid); add!(dh, :u, 1); close!(dh)
+cellvalues = CellScalarValues(QuadratureRule{2, RefCube}(2), Lagrange{2, RefCube, 1}());
+K = create_sparsity_pattern(dh)
+r = zeros(ndofs(dh));
+
+struct ThermalMaterial
+    k::Float64 # Thermal conductivity
+    f::Float64 # Volumetric heat source
+end
+material = ThermalMaterial(1.0, 1.0);
+
+function FerriteAssembly.element_routine!(Ke, re, state, ae,
+        material::ThermalMaterial, cellvalues, cellbuffer
+        )
+    n_basefuncs = getnbasefunctions(cellvalues)
+    # Loop over quadrature points
+    for q_point in 1:getnquadpoints(cellvalues)
+        dΩ = getdetJdV(cellvalues, q_point)
+        for i in 1:n_basefuncs
+            δN  = shape_value(cellvalues, q_point, i)
+            ∇δN = shape_gradient(cellvalues, q_point, i)
+            # Add body load contribution to re
+            re[i] += -material.f*δN * dΩ
+            # Loop over trial shape functions
+            for j in 1:n_basefuncs
+                ∇N = shape_gradient(cellvalues, q_point, j)
+                # Add contribution to Ke
+                Ke[i, j] += material.k*(∇δN ⋅ ∇N) * dΩ
+            end
+        end
+    end
+end;
+
+grid_domain = DomainSpec(dh, material, cellvalues)
+buffer = setup_domainbuffer(grid_domain);
+
+assembler = start_assemble(K, r);
+
+work!(assembler, buffer);
+
+ch = ConstraintHandler(dh)
+faceset = union((getfaceset(grid,k) for k in ("left", "right", "bottom", "top"))...)
+add!(ch, Dirichlet(:u, faceset, Returns(0.0)))
+close!(ch);
+apply_zero!(K, r, ch)
+
+a = -K\r
+vtk_grid("heat_equation", grid) do vtk
+    vtk_point_data(vtk, dh, a)
+end;
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
