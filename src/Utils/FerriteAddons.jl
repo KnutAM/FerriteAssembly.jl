29064ec1c770b47fb66581536d7734d7f607256a
Knut Andreas Meyer <knutam@gmail.com>
2023-10-03 12:49:23 +0200

    Remove getcelltype type piracy

--- src/Utils/FerriteAddons.jl
+++ src/Utils/FerriteAddons.jl
@@ -5,7 +5,6 @@ _getgrid(sdh::SubDofHandler) = sdh.dh.grid
 
 # Type piracy:
 Ferrite.getcellset(sdh::SubDofHandler) = sdh.cellset
-Ferrite.getcelltype(sdh::SubDofHandler) = getcelltype(_getgrid(sdh), first(getcellset(sdh)))
 
 # Custom functions for SubDofHandler
 function create_dofrange(sdh::SubDofHandler)
