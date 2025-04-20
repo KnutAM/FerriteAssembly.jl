b344cd104919d1ddf76efd7bed24b8e7109b3a94
Knut Andreas <knutam@gmail.com>
2025-04-20 17:08:03 +0200

    Download assets from assets branch

---
+++ docs/download_assets.jl
@@ -0,0 +1,19 @@
+using Downloads: download
+
+function download_assets(storepath::String, assets::Vector{String})
+    mkpath(storepath)
+    for asset in assets
+        afile = joinpath(storepath, asset)
+        isfile(afile) || download(FerriteAssembly.asset_url(asset), afile)
+    end
+end
+
+download_assets(
+    joinpath(@__DIR__, "src", "assets"),
+    ["domains.svg"]
+    )
+
+download_assets(
+    joinpath(@__DIR__, "src", "tutorials"),
+    ["mixed_materials.png", "zener.svg"],
+    )
